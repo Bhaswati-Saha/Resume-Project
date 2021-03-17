f90a2ecc08076019764d4ba843f0e0f10d25f92c
Bhaswati-Saha <bhaswati.sh12@gmail.com>
2021-03-18 01:13:45 +0530

    Final commit

--- Home.js
+++ Home.js
@@ -2,14 +2,14 @@ var home=document.querySelectorAll(".nav-link");
 for(var i=0;i<(home.length-1);i++)
 {
 	home[i].addEventListener("click",function(){
-         home[0].removeAttribute("id","content2");
-         home[1].removeAttribute("id","content2");
-         home[2].removeAttribute("id","content2");
-         home[3].removeAttribute("id","content2");
-         home[4].removeAttribute("id","content2");
-         home[5].removeAttribute("id","content2");
-         home[6].removeAttribute("id","content2");
-         home[7].removeAttribute("id","content2");
-         this.setAttribute("id","content2");
-    });
+      home[0].removeAttribute("id","content2");
+      home[1].removeAttribute("id","content2");
+      home[2].removeAttribute("id","content2");
+      home[3].removeAttribute("id","content2");
+      home[4].removeAttribute("id","content2");
+      home[5].removeAttribute("id","content2");
+      home[6].removeAttribute("id","content2");
+      home[7].removeAttribute("id","content2");
+      this.setAttribute("id","content2");
+   });
 }
